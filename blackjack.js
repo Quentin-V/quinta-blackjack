e1748f9888037301761b81a1c8021edacec63740
BuildTools <unconfigured@null.spigotmc.org>
2020-10-07 20:36:10 +0200

    added a give/set command, baltop command, and commented the file

--- blackjack.js
+++ blackjack.js
@@ -9,48 +9,65 @@ const bot = new Discord.Client();
 bot.on('ready', () => {
 	console.log(`Connected as ${bot.user.tag}`);
 })
-var games = [];
+
+var games = []; // Initiates an array for all games if there are
+
 bot.on('message', message => {
 
-	if(message.author.bot) return;
+	if(message.author.bot) return; // If bot, return
 
-	let isInGameChannel = false;
 	let game = null;
-	games.forEach(g => {
+	games.forEach(g => { // Try to find the game where the message has been sent
 		if(g.channel.id === message.channel.id) {
 			isInGameChannel = true;
 			game = g;
 		}
 	});
 
-	if(message.content === 'bjstart' && game === null) {
+	if(message.content === 'bjstart' && game === null) { // Creates a new game if no one was found
 		message.delete();
 		games.push(new BlackJack(message));
 	}
 
-	if(game === null) return;
+	if(game === null) return; // If no new game has been found,
 
-	if(message.content === 'bal') {
+	message.delete(); // Deletes the message
+
+	if(message.content === 'bal') { // Displays the balance of the user sending the message
 		if(message.channel.type === 'text') message.delete();
 		message.reply(game.getBalance(message.author));
 		return;
 	}
 
-	if(!isInGameChannel) return;
-
-	message.delete();
-
-	if(message.content.startsWith('bet') && !game.dealing) {
+	if(message.content.startsWith('bet') && !game.dealing) { // Place a bet
 		game.bet(message);
 	}
 
-	if(message.content === 'deal' && !game.dealing) {
+	if(message.content === 'cancelbet' && !game.dealing) { // Cancel a bet
+		game.cancelBet(message.author);
+	}
+
+	if(message.content === 'deal' && !game.dealing) { // Start dealing
 		game.deal();
 	}
 
-	if(message.content === 'cancelbet' && !game.dealing) {
-		game.cancelBet(message.author);
+	if(message.content.startsWith('baltop')) { // Displays the 10 first players by balance amount
+		let sortedPlayers = game.allPlayers.slice().sort((p1, p2) => p2.balance - p1.balance);
+		sortedPlayers.splice(10);
+		let msgText = `Top 10 players :\n`;
+		sortedPlayers.forEach(p => msgText += `\t${p.user} | ${p.balance}💰`);
+		message.channel.send(msgText);
 	}
+
+	// Admin command to give/set balance for a player
+	if(message.author.id === '184331142286147584' && (message.content.startsWith('give') || message.content.startsWith('set'))) {
+		let amount = parseInt(message.content.split(' ')[2]);
+		let user = message.mentions.users.array()[0];
+		let player = game.players.find(p => p.user.id === user.id);
+		player.balance = message.content.startsWith('give') ? player.balance + amount : amount;
+	}
+
+
 });
 
-bot.login(credentials.token);
+bot.login(credentials.token); // Logs the bot in
